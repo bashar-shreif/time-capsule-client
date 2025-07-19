754b66e548acfed396d76a8e2d9845e5c4919488
Bashar Shreif <basharchreif70@gmail.com>
2025-07-19 18:23:17 +0300

    structuring TimeAtom

--- src/Components/landing_page/TimeAtom.jsx
+++ src/Components/landing_page/TimeAtom.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import CapsuleIllustration from '../../assets/images/CapsuleIllustration.png';
-import Ellipse from '../../assets/images/Ellipse.png';
 
-const SideBar = () => {
+const TimeAtom = () => {
     return (
-        <div>
-            <img src={CapsuleIllustration} alt="time_capsule" />
-            <img src={Ellipse} />
-            <img src={Ellipse} />
-            <img src={Ellipse} />
+        <div className="time-atom">
+            <div className="ring-1">
+                <div className="dot dot-1"></div>
+            </div>
+            <div className="ring-2">
+                <div className="dot dot-2"></div>
+            </div>
+
+            <img className='capsule' src={CapsuleIllustration} alt="time_capsule" />
         </div>
     );
 }
 
-export default SideBar;
+export default TimeAtom;
