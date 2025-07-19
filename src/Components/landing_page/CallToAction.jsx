901b012ba7fe56f753a42c1fd295326417812097
Bashar Shreif <basharchreif70@gmail.com>
2025-07-19 18:21:32 +0300

    structuring Navigator

--- src/Components/landing_page/CallToAction.jsx
+++ src/Components/landing_page/CallToAction.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
 const CallToAction = () => {
-    const navigate = useNavigate();
-
     return (
         <div className='call-to-action'>
             <p className='cta-header'>Send a message to the future.</p>
             <p className='cta-message'>Save a moment, reveal it when the time is right!</p>
-            <button className='register-btn' onClick={() => navigate('/register')}
+            <button className='register-btn'
             >Register</button>
         </div>
     );
--- src/Components/landing_page/Navigator.jsx
+++ src/Components/landing_page/Navigator.jsx
@@ -3,14 +3,15 @@ import Icon from '../../assets/icons/svgs/Icon.svg';
 
 const Navigator = () => {
     return (
-        <div>
-            <ul>
-                <li>
-                    <img src={Icon} alt="capsulock" />
-                    <h1>Capsulock</h1>
+        <div className="navigator">
+            <ul className="nav-list">
+                <li className="nav-icon">
+                    <img src={Icon} alt="capsulock" className="website-icon" />
+                    <h1 className="website-name">Capsulock</h1>
                 </li>
                 <li>
-                    <button>Login</button>
+                    <button className="login-btn"
+                    >Login</button>
                 </li>
             </ul>
         </div>
